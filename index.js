c7a7c3f32831b780284fea240b2165f485a94729
Jaewon Ahn <jaewonahn@Jaewons-MacBook-Pro.local>
2019-06-06 13:34:12 +0900

    made a register function

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://JohnAhn:abc1234@react-blog-dc5kl.mongodb.net/test?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
+
+mongoose.connect('mongodb+srv://jaewon:abc1234@react-blog-dc5kl.mongodb.net/test?retryWrites=true&w=majority',
      {useNewUrlParser: true }).then(() =>console.log('DB connected'))
                               .catch(err => console.error(err));
 
 
-app.get('/', (req,res)=>{
-    res.send('hello world')
-});
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
+
+app.post('/api/users/register', (req, res) => {
+        const user = new User(req.body);
+
+        user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        res.status(200).json({
+            success: true,
+            userData: doc
+        })
+    })
+})
 
 
 
