9f9815cc665073b38375d1e80b4dbd5c5c48b320
Jaewon Ahn <jaewonahn@Jaewons-MacBook-Pro.local>
2019-06-06 19:45:55 +0900

    set up node mon

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/',(req,res)=>{
+    res.json({"hello ~": "Hi ~~ sadlaskdaskd"})
+})
+
 
 app.post('/api/users/register', (req, res) => {
         const user = new User(req.body);
