ca71a527bfd28c838504913dd474e15bbbefec83
John Ahn <smileajw1004@gmail.com>
2019-06-11 21:45:16 +0900

    made login routing

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const config = require('./config/key')
+const config = require("./config/key");
 
-const { User } = require('./models/user');
+const { User } = require("./models/user");
 
-mongoose.connect(config.mongoURI,
-     {useNewUrlParser: true }).then(() =>console.log('DB connected'))
-                              .catch(err => console.error(err));
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
 
-
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.json({"hello ~": "Hi ~~ sadlaskdaskd"})
-})
+app.get("/", (req, res) => {
+  res.json({ "hello ~": "Hi ~~ sadlaskdaskd" });
+});
 
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
 
-app.post('/api/users/register', (req, res) => {
-        const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      userData: doc
+    });
+  });
+});
 
-        user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        res.status(200).json({
-            success: true,
-            userData: doc
-        })
-    })
-})
+app.post("/api/user/login", (req, res) => {
+  //find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found"
+      });
+    //comparePassword
 
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "wrong password" });
+      }
+    });
 
+    //generateToken
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true
+        });
+    });
+  });
+});
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const userSchema = mongoose.Schema({
+const jwt = require('jsonwebtoken');
 
+
+
+const userSchema = mongoose.Schema({
     name: {
         type:String,
         maxlength:50
@@ -51,6 +54,23 @@ userSchema.pre('save', function( next ) {
     }
 });
 
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token =  jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token;
+    user.save(function (err, user){
+        if(err) return cb(err)
+        cb(null, user);
+    })
+}
 
 
 const User = mongoose.model('User', userSchema)
