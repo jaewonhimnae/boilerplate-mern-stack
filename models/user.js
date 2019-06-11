e551b6f44bd0bbd70443a2f7adfda13f7e0d06ab
John Ahn <smileajw1004@gmail.com>
2019-06-11 21:04:03 +0900

    hashed the password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
 
     name: {
@@ -30,6 +31,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+    
+    if(user.isModified('password')){
+
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash 
+            })
+        })
+    } else {
+        next ()
+    }
+});
+
+
 
 const User = mongoose.model('User', userSchema)
 
