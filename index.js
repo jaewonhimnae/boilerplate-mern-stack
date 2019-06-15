c4e53ffbe2c1e32246431060a5675eb8655dc50d
John Ahn <smileajw1004@gmail.com>
2019-06-15 19:36:49 +0900

    logout routing

--- index.js
+++ index.js
@@ -71,4 +71,16 @@ app.post("/api/user/login", (req, res) => {
   });
 });
 
+app.get("/api/user/logout", auth, (req,res) =>{
+  User.findOneAndUpdate({_id: req.user._id}, { token: ""}, (err, doc)=>{
+      if(err) return res.json({ success: false, err })
+      return res.status(200).send({
+        success: true
+      })
+  })
+})
+
+
+
+
 app.listen(5000);
