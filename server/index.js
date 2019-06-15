31a704710d7566bfb585ca9c07270c6f1c3e4c68
John Ahn <smileajw1004@gmail.com>
2019-06-15 20:31:56 +0900

    heroku again

--- server/index.js
+++ server/index.js
@@ -18,6 +18,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
+app.get("/", (req, res)=>{
+  res.json({"hello": " I am happy to deploy our application"})
+})
+
+
+
 app.get("/api/user/auth", auth,  (req,res) =>{
   res.status(200).json({
     _id:req._id,
