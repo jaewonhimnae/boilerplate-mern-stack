26ac5bb458f195ad21158263ee35190da1f9b667
John Ahn <smileajw1004@gmail.com>
2019-06-15 20:23:07 +0900

    initialize heroku deployment

--- server/index.js
+++ server/index.js
@@ -81,6 +81,8 @@ app.get("/api/user/logout", auth, (req,res) =>{
 })
 
 
+const port = process.env.PORT || 5000
 
-
-app.listen(5000);
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`)
+});
