3d3c9b7c78df4325e28942e7bf2edb1b9bd82b3e
John Ahn <smileajw1004@gmail.com>
2019-10-24 20:55:47 +0900

    Fixed

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import { loginUser } from "../../../_actions/user_actions";
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-
+import { Form, Icon, Input, Button, Checkbox, Typography } from 'antd';
 import { useDispatch } from "react-redux";
+
+const { Title } = Typography;
 
 function LoginPage(props) {
   const dispatch = useDispatch();
+  const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
+
+  const [formErrorMessage, setFormErrorMessage] = useState('')
+  const [rememberMe, setRememberMe] = useState(rememberMeChecked)
+
+  const handleRememberMe = () => {
+    setRememberMe(!rememberMe)
+  };
+
+  const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
 
   return (
     <Formik
       initialValues={{
-        email: '',
+        email: initialEmail,
         password: '',
       }}
       validationSchema={Yup.object().shape({
@@ -31,12 +42,25 @@ function LoginPage(props) {
             password: values.password
           };
 
-          dispatch(loginUser(dataToSubmit)).then(response => {
-            if (response.payload.loginSuccess) {
-              props.history.push("/");
-            }
-          });
-
+          dispatch(loginUser(dataToSubmit))
+            .then(response => {
+              if (response.payload.loginSuccess) {
+                if (rememberMe === true) {
+                  window.localStorage.setItem('rememberMe', values.id);
+                } else {
+                  localStorage.removeItem('rememberMe');
+                }
+                props.history.push("/");
+              } else {
+                setFormErrorMessage('Check out your Account or Password again')
+              }
+            })
+            .catch(err => {
+              setFormErrorMessage('Check out your Account or Password again')
+              setTimeout(() => {
+                setFormErrorMessage("")
+              }, 3000);
+            });
           setSubmitting(false);
         }, 500);
       }}
@@ -56,7 +80,7 @@ function LoginPage(props) {
         return (
           <div className="app">
 
-            <h3>Log In</h3>
+            <Title level={2}>Log In</Title>
             <form onSubmit={handleSubmit} style={{ width: '350px' }}>
 
               <Form.Item required>
@@ -95,8 +119,12 @@ function LoginPage(props) {
                 )}
               </Form.Item>
 
+              {formErrorMessage && (
+                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
+              )}
+
               <Form.Item>
-                <Checkbox>Remember me</Checkbox>
+                <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
                 <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
                   forgot password
                   </a>
--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Menu } from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
-class LeftMenu extends Component {
-  render() {
-    return (
-      <Menu mode={this.props.mode}>
-        <Menu.Item key="mail">
-          <a href="/">Home</a>
-        </Menu.Item>
-        <SubMenu title={<span>Blogs</span>}>
-          <MenuItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </MenuItemGroup>
-          <MenuItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </MenuItemGroup>
-        </SubMenu>
-      </Menu>
-    );
-  }
+function LeftMenu(props) {
+  return (
+    <Menu mode={props.mode}>
+    <Menu.Item key="mail">
+      <a href="/">Home</a>
+    </Menu.Item>
+    <SubMenu title={<span>Blogs</span>}>
+      <MenuItemGroup title="Item 1">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      </MenuItemGroup>
+      <MenuItemGroup title="Item 2">
+        <Menu.Item key="setting:3">Option 3</Menu.Item>
+        <Menu.Item key="setting:4">Option 4</Menu.Item>
+      </MenuItemGroup>
+    </SubMenu>
+  </Menu>
+  )
 }
 
-export default LeftMenu;
+export default LeftMenu
