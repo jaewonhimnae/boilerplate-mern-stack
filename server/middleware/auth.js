bde3e9abe19d439a716adb2f72ed57059f77247f
John Ahn <smileajw1004@gmail.com>
2019-12-24 15:28:09 +0900

    changed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/User');
+const { User } = require('../models/user');
 
 let auth = (req, res, next) => {
   let token = req.cookies.w_auth;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/User");
+const { User } = require("../models/user");
 
 const { auth } = require("../middleware/auth");
 
