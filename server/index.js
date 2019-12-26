d743b72e077a21a2232952f0ca421506ff4df6e5
John Ahn <smileajw1004@gmail.com>
2019-12-26 09:33:15 +0900

    file path changed in index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user');
+const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
   let token = req.cookies.w_auth;
