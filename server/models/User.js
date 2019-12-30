163578cfb6af27c86d5c1fe744b8184643c7a20a
John Ahn <smileajw1004@gmail.com>
2019-12-30 20:06:46 +0900

    token Exp Fixed

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
+const moment = require("moment");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -64,7 +65,9 @@ userSchema.methods.comparePassword = function(plainPassword,cb){
 userSchema.methods.generateToken = function(cb) {
     var user = this;
     var token =  jwt.sign(user._id.toHexString(),'secret')
+    var oneHour = moment().add(1, 'hour').valueOf();
 
+    user.tokenExp = oneHour;
     user.token = token;
     user.save(function (err, user){
         if(err) return cb(err)
