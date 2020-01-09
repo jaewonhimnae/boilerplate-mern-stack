e501a14f9d9c1eb9e54c156dc22eb3ea505cd1a3
John Ahn <smileajw1004@gmail.com>
2020-01-09 17:00:30 +0900

    mongoose warning fixed , saslprep downloaded

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ const config = require("./config/key");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(config.mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
