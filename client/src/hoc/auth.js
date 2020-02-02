518f1549fdd0722d7b4f2e317301cd073f703c62
John Ahn <smileajw1004@gmail.com>
2020-02-02 19:39:31 +0900

    small changes

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { auth } from '../_actions/user_actions';
 import { useSelector, useDispatch } from "react-redux";
@@ -27,7 +28,7 @@ export default function (ComposedClass, reload, adminRoute = null) {
                 }
             })
             
-        }, [dispatch, props.history, user.googleAuth])
+        }, [])
 
         return (
             <ComposedClass {...props} user={user} />
