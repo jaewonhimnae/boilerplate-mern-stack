76003db7ccbebfccad7e9f1087c98a781ec1d6fd
Song Ahyeon <song@5works.co.kr>
2022-12-27 11:47:18 +0900

    #4 SSL setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,11 +4,17 @@ import { FaCode } from "react-icons/fa";
 function LandingPage() {
     return (
         <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{width: '100%', margin: '0'}}>
+            {/* Main Image */}
+            <div style={{width: '85%', margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr />
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+
+        </div>
         </>
     )
 }
